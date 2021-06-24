1315572c64c76e9f4524f11ba0c982df41b79ebe
nghiamx49 <mxnghia49@gmail.com>
2021-06-24 17:41:03 +0700

    +add project contants

---
+++ src/apiServices/authenticateService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const authenticateService = {
+  login: async () => {},
+  getUserDetail: async () => {},
+  logout: async () => {},
+  forgotPassword: async () => {},
+  registerAsUser: async () => {},
+  registerAsProvider: async () => {},
+};
+
+export default authenticateService;
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,16 +1,54 @@
 const initialState = {
   listProducts: [
     {
-      product_id: 1,
-      product_name: "Ao Quan Tre Em",
-      quantity_purchased: 2,
-      unit_price: 150000,
+      id: 0,
+      shopName: "shop Hoang 0",
+      linkImage:
+        "https://cdn.mediamart.vn/Product/tai-nghe-chup-dau-co-mic-kanen-ip2050-mau-do-den-y72Dcj.png",
+      name: "tai nghe 0 day",
+      price: 100,
+      number: 1,
+      status: false,
     },
     {
-      product_id: 2,
-      product_name: "Ao Quan Nguoi Lon",
-      quantity_purchased: 3,
-      unit_price: 150000,
+      id: 1,
+      shopName: "shop Hoang 1",
+      linkImage:
+        "https://cdn.mediamart.vn/Product/tai-nghe-chup-dau-co-mic-kanen-ip2050-mau-do-den-y72Dcj.png",
+      name: "tai nghe 0 day",
+      price: 100,
+      number: 1,
+      status: false,
+    },
+    {
+      id: 2,
+      shopName: "shop Hoang 2",
+      linkImage:
+        "https://cdn.mediamart.vn/Product/tai-nghe-chup-dau-co-mic-kanen-ip2050-mau-do-den-y72Dcj.png",
+      name: "tai nghe 0 day",
+      price: 100,
+      number: 1,
+      status: false,
+    },
+    {
+      id: 3,
+      shopName: "shop Hoang 3",
+      linkImage:
+        "https://cdn.mediamart.vn/Product/tai-nghe-chup-dau-co-mic-kanen-ip2050-mau-do-den-y72Dcj.png",
+      name: "tai nghe 0 day",
+      price: 100,
+      number: 1,
+      status: false,
+    },
+    {
+      id: 4,
+      shopName: "shop Hoang 4",
+      linkImage:
+        "https://cdn.mediamart.vn/Product/tai-nghe-chup-dau-co-mic-kanen-ip2050-mau-do-den-y72Dcj.png",
+      name: "tai nghe 0 day",
+      price: 100,
+      number: 1,
+      status: false,
     },
   ],
   total: 750000,
