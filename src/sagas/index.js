9ccaf52ca87383b2ee90354c201ca72c00a3a875
nghiamx49 <mxnghia49@gmail.com>
2021-07-14 17:09:47 +0700

    fix saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,13 +1,21 @@
 import authenticateSaga from "./authenticateSaga";
-import adminSaga from "./adminSaga"
+import adminSaga from "./adminSaga";
 import cartSaga from "./cartSaga";
 import { all } from "redux-saga/effects";
 import getListProductSaga from "./providerSaga";
 import getProductsSaga from "./productsaga";
-import userSaga from './userSaga'
+import userSaga from "./userSaga";
 export default function* rootSaga() {
   /*all: khá tương đồng với Promise.all. 
     Nó sẽ cho phép tất cả các tasks chạy song song và đợi tất cả chúng trả về kết quả mới kết thúc
     */
-  yield all([authenticateSaga(), cartSaga(),getProductsSaga(), userSaga(), adminSaga(), cartSaga(),getListProductSaga(),getProductsSaga()]);
+  yield all([
+    authenticateSaga(),
+    cartSaga(),
+    getProductsSaga(),
+    userSaga(),
+    adminSaga(),
+    getListProductSaga(),
+    getProductsSaga(),
+  ]);
 }
